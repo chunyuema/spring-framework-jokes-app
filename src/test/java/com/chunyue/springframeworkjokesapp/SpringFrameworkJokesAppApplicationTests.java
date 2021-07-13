45e4d59be1649ed71f26feed7c35fa3aeda439ba
Chunyue Ma <cma01@wesleyan.edu>
2021-07-12 22:02:52 -0400

    initial commit with spring project

---
+++ src/test/java/com/chunyue/springframeworkjokesapp/SpringFrameworkJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chunyue.springframeworkjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkJokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
